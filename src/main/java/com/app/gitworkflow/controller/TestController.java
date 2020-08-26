732d35bcbdf0550525db7b04f5fd5b33b89fdf5e
Janecience <janescience.top@gmail.com>
2020-08-26 21:30:40 +0700

    [#FEA01-01] Fix add method Test

--- src/main/java/com/app/gitworkflow/controller/TestController.java
+++ src/main/java/com/app/gitworkflow/controller/TestController.java
@@ -1,4 +1,13 @@
 package com.app.gitworkflow.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/test")
+    public void Test(){
+
+    }
 }
