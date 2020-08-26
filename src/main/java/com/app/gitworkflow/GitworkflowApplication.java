09d96aac69043272effcb68b0b95100cdedc8e93
Janecience <janescience.top@gmail.com>
2020-08-26 21:00:39 +0700

    ini project

---
+++ src/main/java/com/app/gitworkflow/GitworkflowApplication.java
@@ -0,0 +1,13 @@
+package com.app.gitworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitworkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitworkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/gitworkflow/GitworkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gitworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitworkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
